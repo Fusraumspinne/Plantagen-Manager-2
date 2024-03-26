a21900cf186714f0565a2244ac886ce0d238fe79
Fusraumspinne <mjjaeger2002@gmail.com>
2024-03-26 15:49:44 +0100

    hauptmnu erweiterung

--- Plantagen Manager 2/Assets/Script/Hauptmenu.cs
+++ Plantagen Manager 2/Assets/Script/Hauptmenu.cs
@@ -2,9 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Hauptmenu : MonoBehaviour
 {
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject steuerung;
+    [SerializeField] private GameObject einstellungen;
+
+    [SerializeField] private Slider slider;
+
+    public void OnSliderValueChanged()
+    {
+        float roundedValue = Mathf.Round(slider.value * 100f) / 100f;
+        Debug.Log("Aktueller Slider-Wert: " + roundedValue);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            if (steuerung.activeSelf)
+            {
+                steuerung.SetActive(false);
+                menu.SetActive(true);
+            }
+            else if (einstellungen.activeSelf)
+            {
+                einstellungen.SetActive(false);
+                menu.SetActive(true);
+            }
+        }
+    }
+
+    public void SteuerungPanel()
+    {
+        steuerung.SetActive(true);
+        menu.SetActive(false);
+    }
+
+    public void EinstellungenPanel()
+    {
+        einstellungen.SetActive(true);
+        menu.SetActive(false);
+    }
+
     public void NeuesSpiel()
     {
         PlayerPrefs.SetInt("NeuesGame", 1);
